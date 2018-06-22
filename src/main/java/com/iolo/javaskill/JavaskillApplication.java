1e8de87184b75f07834c25473445ec09278d0bf8
iOLO <516502968@qq.com>
2018-06-22 08:46:33 +0800

    first push

---
+++ src/main/java/com/iolo/javaskill/JavaskillApplication.java
@@ -0,0 +1,12 @@
+package com.iolo.javaskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaskillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaskillApplication.class, args);
+    }
+}
